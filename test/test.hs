59662bfd3d0096733541053f85b6942de49b2aef
David Hanley <dhanley@groupon.com>
2022-07-30 20:43:04 -0700

    got testing working

--- test/test.hs
+++ test/test.hs
@@ -1,15 +1,25 @@
 import Test.HUnit
 
 import Scoring
+--import Data.Char
+
+--test1 :: Test
+--test1 = TestCase (assertEqual "test1" (stringToGender "Male") (Just Male) )
+
+--test2 :: Test
+--test2 = TestCase (assertEqual "test1" (stringToGender "m") (Just Male) )
 
 test2 :: Test
-test2 = TestCase (assertEqual "test2" (stringToGender "Male") (Just Male) )
-
-tests :: Test
-tests = TestList [TestLabel "test1" test2]
+test2 = TestList [ "First STG test"   ~: (Just Male) ~=? (stringToGender "Male")
+                 , "Second STG test"  ~: (Just Male) ~=? (stringToGender "m")
+                 , "Third STG test"  ~: (Just Female) ~=? (stringToGender "f")
+                 , "Fourth STG test"  ~: (Just Female) ~=? (stringToGender "Female")
+                 , "Fifth STG test"  ~: Nothing ~=? (stringToGender "Bobby")
+                 , "Sixth STG test"  ~: Nothing ~=? (stringToGender "")
+                  ]
 
 main :: IO Counts
 main = do
-  runTestTT tests
+  runTestTT test2
 
 
