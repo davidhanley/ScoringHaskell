50dd98751e7f95b4894058b818a7320450b93ff0
David Hanley <dhanley@groupon.com>
2022-07-31 19:42:40 -0700

    parsing some fields for real, even dates

--- app/Scoring.hs
+++ app/Scoring.hs
@@ -4,6 +4,15 @@ import System.Directory (listDirectory)
 import Text.Read (readMaybe)
 import Data.Strings (strSplitAll)
 import Data.Maybe (mapMaybe)
+import Data.Time
+
+import Data.Int
+import Data.Ratio
+
+--produce an infinite list of score factors
+scoreFor :: Integer -> Rational
+scoreFor x = 5 % (4+x) :: Rational
+scores = map scoreFor [1..]
 
 data Gender = Male | Female deriving (Show, Eq )
 
@@ -19,20 +28,22 @@ data Athlete = Athlete { athleteName :: String , age :: Maybe Int , sex :: Maybe
 athFromString :: String -> Maybe Athlete
 athFromString line =
   case (strSplitAll "," line) of
-    (_:name:age:sex:_) -> Just $ Athlete { athleteName = name ,age = (readMaybe age::Maybe Int)  , sex = stringToGender sex }
+    (_:name:age:sex:_) -> Just $ Athlete { athleteName = name, age = (readMaybe age::Maybe Int), sex = stringToGender sex }
     _ -> Nothing
 
-
-
-data RaceData = RaceData { raceName :: String , raceDate :: String , points :: Int , athLines :: [Athlete]} deriving (Show)
+data RaceData = RaceData { raceName :: String , raceDate :: Day , points :: Int , athLines :: [Athlete]} deriving (Show)
 
 path :: String
 path = "data"
 
+parseDate :: String -> Day
+parseDate dateString = case (mapMaybe (\s -> (readMaybe s)::Maybe Int) (strSplitAll "-" dateString)) of
+  [y,m,d] -> fromGregorian (toInteger y) m d
+
 processHeader:: [String] -> Maybe RaceData
 processHeader (name:date:_:p2:rest) =
   case (readMaybe p2) of
-    Just(x) -> Just $ RaceData { raceName=name, raceDate = date , points = x , athLines = mapMaybe athFromString rest }
+    Just(x) -> Just $ RaceData { raceName=name, raceDate = parseDate date , points = x , athLines = mapMaybe athFromString rest }
     _ -> Nothing
 processHeader _ = Nothing
 
