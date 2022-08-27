ab51c93fe176a64dd9f003d064dfd3888e95d01a
David Hanley <dhanley@groupon.com>
2022-08-27 12:45:50 -0700

    unboxing athletes and sorting

--- app/Scoring.hs
+++ app/Scoring.hs
@@ -5,6 +5,8 @@ import Text.Read (readMaybe)
 import Data.Strings (strSplitAll)
 import Data.Maybe (mapMaybe)
 import Data.Time
+import Data.List
+import qualified Data.Map as Map
 
 import Data.Int
 import Data.Ratio
@@ -14,7 +16,7 @@ scoreFor :: Integer -> Rational
 scoreFor x = 5 % (4+x) :: Rational
 scores = map scoreFor [1..]
 
-data Gender = Male | Female deriving (Show, Eq )
+data Gender = Male | Female deriving (Show, Eq, Ord)
 
 stringToGender :: String -> Maybe Gender
 stringToGender ('m':_) = Just Male
@@ -23,12 +25,20 @@ stringToGender ('f':_) = Just Female
 stringToGender ('F':_) = Just Female
 stringToGender _ = Just Female
 
-data Athlete = Athlete { athleteName :: String , age :: Maybe Int , sex :: Maybe Gender } deriving (Show, Eq )
+data Athlete = Athlete { athleteName :: String, age :: Maybe Int, sex :: Gender, frgn :: Bool } deriving (Show, Eq, Ord)
+
+
+closeEnough :: Int -> Int -> Bool
+closeEnough i1 i2 = (i1 == i2) || (i1+1 == i2) || (i1-1 == i2)
+
 
 athFromString :: String -> Maybe Athlete
 athFromString line =
   case (strSplitAll "," line) of
-    (_:name:age:sex:_) -> Just $ Athlete { athleteName = name, age = (readMaybe age::Maybe Int), sex = stringToGender sex }
+    (_:name:age:stringSex:_) ->
+      case (stringToGender stringSex) of
+          Just(gender) -> Just $ Athlete { athleteName = name, age = (readMaybe age::Maybe Int), sex = gender, frgn = True}
+          _ -> Nothing
     _ -> Nothing
 
 data RaceData = RaceData { raceName :: String , raceDate :: Day , points :: Int , athLines :: [Athlete]} deriving (Show)
@@ -40,22 +50,23 @@ parseDate :: String -> Day
 parseDate dateString = case (mapMaybe (\s -> (readMaybe s)::Maybe Int) (strSplitAll "-" dateString)) of
   [y,m,d] -> fromGregorian (toInteger y) m d
 
-processHeader:: [String] -> Maybe RaceData
+processHeader :: [String] -> Maybe RaceData
 processHeader (name:date:_:p2:rest) =
   case (readMaybe p2) of
-    Just(x) -> Just $ RaceData { raceName=name, raceDate = parseDate date , points = x , athLines = mapMaybe athFromString rest }
+    Just(x) -> Just $ RaceData { raceName = name, raceDate = parseDate date , points = x , athLines = mapMaybe athFromString rest }
     _ -> Nothing
 processHeader _ = Nothing
 
-readAFile:: String -> IO (Maybe RaceData)
+readAFile :: String -> IO (Maybe RaceData)
 readAFile fn = do
   let fullpath = (path ++ "/" ++ fn)
   input <- readFile fullpath
   return (processHeader (lines input))
 
+
 scoring :: IO ()
 scoring = do
   files <- listDirectory path
-  res <- traverse readAFile files
-  print res
+  races <- traverse readAFile files
+  print (sort (concat (map athLines (mapMaybe (\x -> x ) races))))
 
